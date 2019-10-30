ae0c9a0af4591013c5809e4ced434b8c65770496
Shehryar Ehtesham <se318@rutgers.edu>
2019-10-30 16:39:34 -0400

    modified liribot.js

--- liri.js
+++ liri.js
@@ -1,7 +1,49 @@
 require("dotenv").config();
 var fs = require('fs');
+var Spotify = require('node-spotify-api');
 var keys = require("./keys.js");
+var request = require('request');
 
 
 
 var spotify = new Spotify(keys.spotify);
+var omdbKey = keys.omdb.api_key;
+var bandsInTown = keys.bandsInTown
+
+var command = process.argv[2];
+var userInput = process.argv[3];
+
+switch (command){
+    case ('spotify-this-song'):
+        if(userInput){
+            spotifyThisSong(userInput);
+        } else{
+            spotifyThisSong("My Heart Will Go On");
+        };
+    break;
+    case ('movie-this'):
+        if(userInput){
+            omdb(userInput)
+        } else {
+            omdb('Mr. Nobody.');
+        }
+    break;
+    case ('do-what-it-says '):
+            doWhatItSays()
+    break;
+    default:
+        console.log('Please Try Again');
+    
+    };
+
+
+
+
+
+function spotifyThisSong(song){};
+
+var omdbKey = "5124890f";
+
+function omdb(movie){};
+
+function doWhatItSays(){};
